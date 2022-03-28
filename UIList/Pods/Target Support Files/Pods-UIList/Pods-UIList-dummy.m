76341da0e86f46563a25271541532ee09e341adc
Luiz Eduardo <luizdudumr@gmail.com>
2022-03-28 15:55:01 -0300

    UIList Settings initialized and Firebase Setup

---
+++ UIList/Pods/Target Support Files/Pods-UIList/Pods-UIList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIList : NSObject
+@end
+@implementation PodsDummy_Pods_UIList
+@end
---
+++ UIList/UIList/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UIList
+//
+//  Created by Luiz Eduardo Mello dos Reis on 28/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
